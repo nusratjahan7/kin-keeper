6cf44bd02911b9394bdf21093020555adb4ca76b
nusratjahan7 <nusratjahan77222@gmail.com>
2026-04-13 21:39:05 +0600

    make navbar responsive

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/Components/Homepage/Navbar";
+import Navbar from "@/Components/Shared/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
