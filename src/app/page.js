81b696787b78a0cc77ed89052480dd5cb684972d
nusratjahan7 <nusratjahan77222@gmail.com>
2026-04-14 10:12:14 +0600

    Add more friends to the friend list with updated details and contact statuses and finished banner

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Banner from "@/Components/Homepage/Banner";
 
 export default function Home() {
   return (
     <div>
-     
+        <Banner />
     </div>
   );
 }
--- src/lib/getFrnd.js
+++ src/lib/getFrnd.js
@@ -1,8 +1,8 @@
 import { readFileSync } from "fs";
 import { join } from "path";
 
-export function getApps() {
-  const filePath = join(process.cwd(), "data", "data.json");
+export function getFriend() {
+  const filePath = join(process.cwd(), "public", "data.json");
   const fileContents = readFileSync(filePath, "utf8");
   return JSON.parse(fileContents);
 }
